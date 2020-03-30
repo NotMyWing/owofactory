56f88ba02c4817c11ac41780033c47732d70217a
Neeve01 <winwyv@gmail.com>
2020-03-30 13:10:58 +1100

    Owoify File Paths

--- gulpfile.js
+++ gulpfile.js
@@ -2,14 +2,23 @@ const { src, dest, series } = require("gulp");
 const through2 = require("through2");
 const owoify = require("owoify-js").default;
 const del = require("del");
+const rename = require("gulp-rename");
 
-const compileMd = () => {
+const owoContents = () => {
 	return through2.obj((file, _, cb) => {
 		if (file.isBuffer()) {
 			file.contents = Buffer.from(owoify(file.contents.toString()));
 			cb(null, file);
 		}
 	})
+}
+
+const owoPath = () => {
+	return rename((path) => {
+		path.dirname = owoify(path.dirname);
+		path.basename = owoify(path.basename);
+		return path;
+	});
 }
 
 function rmrf(cb) {
@@ -20,12 +29,14 @@ function rmrf(cb) {
 
 function buildMd() {
 	return src("src/guides/**/*.md")
-		.pipe(compileMd())
+		.pipe(owoContents())
+		.pipe(owoPath())
 		.pipe(dest("dest/guides/"));
 }
 
 function copy() {
 	return src(["src/**", "!src/**/*.md"])
+		.pipe(owoPath())
 		.pipe(dest("dest/"));
 }
 
